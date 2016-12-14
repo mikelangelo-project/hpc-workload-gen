9186625bdbad1a7daaf8b982bdbea6870e989be9
Uwe Schilling <uwe.schilling@hlrs.de>
2016-12-14 13:42:00 +0100

    Changed and extended the input parsing
    Change-Id: Idd911d022d11d50b990f63fb7d20f5ccc5417d46
    Signed-off-by: Uwe Schilling <uwe.schilling@hlrs.de>

--- run.py
+++ run.py
@@ -6,13 +6,12 @@
 
 
 import logging
+import click
 from api.ci import Workload
 from api.connection import HLRS
-from getopt import getopt, GetoptError
-from sys import exit, argv
 
 
-def get_logger(debug_lvl='DEBUG'):
+def get_logger():
     """Setup the global logger."""
     # log setup
     logger = logging.getLogger(__name__)
@@ -60,53 +59,25 @@ def stage_out(job_id, conn, workload_generator, logger):
     logger.warn('skipping for the moment')
 
 
-def usage():
-    """Print the usage of this script."""
-    print(
-        'This is the Workload generatar to submit job to HLRS\n'
-        'run.py -w <workload_path.yaml> -m <mongodb://mongodb:27017>')
-
-
-def extract_args(argv, logger):
-    """Extract the provided arguments."""
-    logger.debug('Trying to extract the needed arguments. -> %s' % argv)
-    arguments = {}
-    try:
-        opts, args = getopt(argv[1:], "hw:m:", ["help", "workload="])
-        logger.debug('getopt return: \nopts: %s\n args: %s' % (opts, args))
-    except GetoptError as err:
-        logger.error('Arg parsing failed! Exiting !!!')
-        logger.debug('Revised error:\n%s' % str(err))
-        usage()
-        exit(2)
-    logger.debug('Loop over opts: %s' % opts)
-    for opt, arg in opts:
-        logger.debug('opt: %s arg: %s' % (opt, arg))
-        if opt in ("-h", "--help"):
-            logger.warn('found help. Printing usage and exit.')
-            usage()
-            exit()
-        elif opt in ("-w", "--workload"):
-            arguments['workload'] = arg.rstrip("/")
-            logger.info('found workload: %s' % arguments['workload'])
-        else:
-            logger.warn('no suitable arg found. print usage and exit.')
-            usage()
-            exit(2)
-    logger.debug('returning found args')
-    return arguments['workload']
-
-
-def main():
+@click.command()
+@click.option(
+    '-w',
+    '--workload',
+    type=click.Path(exists=True),
+    help='the path to the workload.yaml',
+    required=True)
+@click.option(
+    '-d',
+    '--datadir',
+    type=click.Path(exists=True),
+    help='base path containing the experiment',
+    required=True)
+def main(workload, datadir):
     """The main program."""
-    debug_lvl = 'INFO'
-    logger = get_logger(debug_lvl)
-
-    # get arguments
-    provided_args = extract_args(argv, logger)
+    logger = get_logger()
 
     # Pars yuml
-    workload_generator = Workload(provided_args, debug_lvl)
+    workload_generator = Workload(workload)
 
     # Build connection
     conn = HLRS()
