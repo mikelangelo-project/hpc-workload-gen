07cf406972ecae46e49af0f900b1f2ac1e8bb606
Nico Struckmann <struckmann@hlrs.de>
2018-01-30 13:24:55 +0100

    Import fixed.
    Change-Id: Ia1f448f3cdba18e43b889d6513dd641bc968f2fa
    Signed-off-by: Nico Struckmann <struckmann@hlrs.de>

--- api/hpc_backend.py
+++ api/hpc_backend.py
@@ -27,7 +27,7 @@ import os
 from time import time, sleep
 from sh import ssh, ErrorReturnCode, rsync
 
-from api.logger import getLogger, reduceLogLevel
+from api.logger import getLogger, setLogLevel
 from api.hpc_config import HPCBackendConfiguration
 
 
@@ -51,7 +51,7 @@ class HPCBackend(object):
             self.ssh_conn = ssh.bake('-p', self.hpcConfig.get_value('ssh_port'))
             self.ssh_conn = ssh.bake('-i', self.hpcConfig.get_value('ssh_key'))
             self.ssh_conn = ssh.bake(self.hpcConfig.get_value('host'))
-            reduceLogLevel()
+            setLogLevel()
         except ErrorReturnCode as e:
             self.logger.error('SSH initialization failed:\n{}'.format(e.stderr))
             sys.exit(1)
